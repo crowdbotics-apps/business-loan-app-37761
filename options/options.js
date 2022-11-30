36c5b3d33b3aa351c3c52fb0b6301d65492029de
MICHELLE TIDWELL <pfgwilshire@gmail.com>
2022-11-30 14:22:05 -0700

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native";
-
 export const globalOptions = {
   name: "business_loan_app_37761",
   url: "https://business_loan_app_37761.botics.co",
   api: "https://business_loan_app_37761.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     "copy": "Routes available!"
   }
-}
+};
---
+++ screens/Untitled1/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Untitled1 = () => {
+  return <View style={_styles.MklPGaIA}>
+      <View style={_styles.Osudxczf}></View></View>;
+};
+
+export default Untitled1;
+
+const _styles = StyleSheet.create({
+  MklPGaIA: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%",
+    borderRadius: 0
+  },
+  Osudxczf: {
+    height: 60,
+    width: 140,
+    backgroundColor: "#e3e3e3",
+    borderRadius: 0,
+    color: "#777777",
+    position: "absolute",
+    left: 108.5
+  }
+});
