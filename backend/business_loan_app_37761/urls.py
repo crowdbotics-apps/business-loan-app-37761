c5518e09f0f396aefe6ff9902c7006894d03d7e8
Crowdbotics <team@crowdbotics.com>
2022-11-30 20:28:43 +0000

    Update admin title

--- backend/business_loan_app_37761/urls.py
+++ backend/business_loan_app_37761/urls.py
@@ -35,9 +35,9 @@ urlpatterns = [
     path("rest-auth/registration/", include("rest_auth.registration.urls")),
 ]
 
-admin.site.site_header = "Business Loan App"
-admin.site.site_title = "Business Loan App Admin Portal"
-admin.site.index_title = "Business Loan App Admin"
+admin.site.site_header = "Business Loan Choices App"
+admin.site.site_title = "Business Loan Choices App Admin Portal"
+admin.site.index_title = "Business Loan Choices App Admin"
 
 # swagger
 api_info = openapi.Info(
